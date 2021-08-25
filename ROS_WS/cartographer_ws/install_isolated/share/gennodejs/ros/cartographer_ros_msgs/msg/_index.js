7181d804609b845682c0edf3f1baf4e3be6c193b
Lilinxiang <987197949@qq.com>
2021-08-25 14:32:58 +0800

    submit version --1

---
+++ ROS_WS/cartographer_ws/install_isolated/share/gennodejs/ros/cartographer_ros_msgs/msg/_index.js
@@ -0,0 +1,24 @@
+
+"use strict";
+
+let SensorTopics = require('./SensorTopics.js');
+let SubmapTexture = require('./SubmapTexture.js');
+let LandmarkList = require('./LandmarkList.js');
+let TrajectoryOptions = require('./TrajectoryOptions.js');
+let StatusResponse = require('./StatusResponse.js');
+let LandmarkEntry = require('./LandmarkEntry.js');
+let StatusCode = require('./StatusCode.js');
+let SubmapList = require('./SubmapList.js');
+let SubmapEntry = require('./SubmapEntry.js');
+
+module.exports = {
+  SensorTopics: SensorTopics,
+  SubmapTexture: SubmapTexture,
+  LandmarkList: LandmarkList,
+  TrajectoryOptions: TrajectoryOptions,
+  StatusResponse: StatusResponse,
+  LandmarkEntry: LandmarkEntry,
+  StatusCode: StatusCode,
+  SubmapList: SubmapList,
+  SubmapEntry: SubmapEntry,
+};
